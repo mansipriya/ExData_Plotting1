b2ad6bc6f277a2b27902ee64388138e84fd47d88
mansipriya <mail4mansipriya@gmail.com>
2018-08-03 18:03:45 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,6 +4,8 @@ t <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
 #susetting the data
 ta <- subset(t,Date=="1/2/2007" | Date =="2/2/2007")
 
+png(filename = "plot1.png", width = 480, height = 480)
+
 #calling the basic plot function
 hist(as.numeric(as.character(ta$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-
+dev.off()
