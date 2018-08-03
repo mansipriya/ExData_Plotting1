6077ccc5294295658832e74a39fe5279923fd99b
mansipriya <mail4mansipriya@gmail.com>
2018-08-03 18:05:36 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -10,9 +10,11 @@ ta$Time <- strptime(ta$Time, format="%H:%M:%S")
 ta[1:1440,"Time"] <- format(ta[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 ta[1441:2880,"Time"] <- format(ta[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
+png(filename = "plot3.png", width = 480, height = 480)
 #plotting
 plot(ta$Time,as.numeric(as.character(ta$Sub_metering_1)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 lines(ta$Time,as.numeric(as.character(ta$Sub_metering_2)),col="red")
 lines(ta$Time,as.numeric(as.character(ta$Sub_metering_3)),col="blue")
 
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
